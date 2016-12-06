0bace544f44a7036626d9e7703775fdcc868cc6c
Andrew S <andrews54757@gmail.com>
2016-12-05 23:04:20 -0500

    Update

--- assets/list.js
+++ assets/list.js
@@ -1,23 +1,38 @@
 "use strict"
 module.exports = class list {
-    constructor(main, title, options, calls) {
+    constructor(main, title, options) {
         this.main = main;
+        this.vis = main.visual
         this.title = title;
         this.options = options
-        this.calls = calls
+
+        this.chosen = 0;
     }
 
     onKey(key) {
         switch (key) {
         case "UP":
-
+            if (this.chosen <= 0) return
+            this.chosen--;
             break;
         case "DOWN":
-
+            if (this.chosen >= this.options.length - 1) return
+            this.chosen++;
             break;
         case "ENTER":
 
+            return
+            break;
+        default:
+            return
             break;
         }
+        this.update()
+    }
+
+    update() {
+        var a = Math.floor(this.vis.height / 2) - this.options.length - 2
+        this.vis.setRow()
+        for (var i = 0; i < this.options.length)
     }
 }
