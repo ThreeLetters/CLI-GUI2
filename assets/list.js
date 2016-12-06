aebc088a135031cc76b13627e6ef126b6be627c0
Andrew S <andrews54757@gmail.com>
2016-12-05 22:49:05 -0500

    Update

--- assets/list.js
+++ assets/list.js
@@ -1,10 +1,23 @@
 "use strict"
 module.exports = class list {
-    constructor(main) {
+    constructor(main, title, options, calls) {
         this.main = main;
+        this.title = title;
+        this.options = options
+        this.calls = calls
     }
 
-    onKey() {
+    onKey(key) {
+        switch (key) {
+        case "UP":
 
+            break;
+        case "DOWN":
+
+            break;
+        case "ENTER":
+
+            break;
+        }
     }
 }
